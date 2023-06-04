a07089e173c7c0d46ba7157135a90f452eacc7d6
0xterran <0xterran@protonmail.com>
2023-06-04 18:08:04 +0700

    graphql setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { hi } from "@/hooks/graphql.hook";
 
 function App() {
+  console.log(hi);
   return (
     <div className="App">
       <header className="App-header">
